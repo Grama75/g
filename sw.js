38f43ee22e34f938166aaeca665f8ccb498ebd33
Grama75 <3257406400@qq.com>
2026-09-19 18:14:06 +0800

    创建 sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+var CACHE='mindmap-v1';
+var FILES=['./','./index.html','./manifest.json','./icon.svg'];
+
+self.addEventListener('install',function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES);}).catch(function(){}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.map(function(k){if(k!==CACHE)return caches.delete(k);}));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',function(e){
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    caches.match(e.request).then(function(r){
+      if(r)return r;
+      return fetch(e.request).then(function(res){
+        if(res&&res.status===200&&res.type==='basic'){
+          var clone=res.clone();
+          caches.open(CACHE).then(function(c){c.put(e.request,clone);});
+        }
+        return res;
+      }).catch(function(){return caches.match('./index.html');});
+    })
+  );
+});
