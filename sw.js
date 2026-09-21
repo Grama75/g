957111dd2cc3f503bdea63b1ef91a333436cc39c
Grama75 <3257406400@qq.com>
2026-09-21 20:10:07 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 var CACHE='mindmap-v1';
-var FILES=['./','./index.html','./manifest.json','./icon.svg'];
-
+var FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES);}).catch(function(){}));
   self.skipWaiting();
